6b14107da63346b85c953d3af915541c8af20d95
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2021-02-22 17:05:07 +0900

    Fix string format

--- Plugins/SamplePlugin/Source/SamplePlugin/SamplePlugin.Build.cs
+++ Plugins/SamplePlugin/Source/SamplePlugin/SamplePlugin.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -23,7 +23,7 @@ public class SamplePlugin : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				"LevelEditor",	// �ǉ�.
+				"LevelEditor",	// 追加.
 			}
 			);
 	}
